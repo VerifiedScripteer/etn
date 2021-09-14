53a3963bcae7a7061c1a70cd724273da790c4423
Dean <deankoesal6@gmail.com>
2021-09-14 21:58:44 +0200

    Create hallo.js

---
+++ commands/hallo.js
@@ -0,0 +1,15 @@
+const discord = require("discord.js")
+
+module.exports.run = async (client, message, args) => {
+
+    var embed = new discord.MessageEmbed()
+        .setDescription(`Hallo ${message.author.tag}!`)
+        .setColor("RANDOM");
+    
+    message.channel.send(embed);
+
+
+}
+module.exports.help = {
+    name: "verlof",
+}
